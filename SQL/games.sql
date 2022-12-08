caf776090375dd1771ddfbb53a5cce41c80b59e7
Andrew Connally <ajconnally93@gmail.com>
2022-12-07 19:40:17 -0600

    created Model files, referenced db, created empty dbs userData.json & projectData.json

--- SQL/games.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO games (title,genre,release_date,review_link)
-
-VALUES 
-("Game Title 1","Genre 1","Release_date 1","https://www.store.steampowered.com"),
-("Game Title 2","Genre 2","Release_date 2","https://www.store.steampowered.com"),
-(),
-(),
-(),
--- models/index.js
+++ models/index.js
@@ -1,1 +1,13 @@
-//initial commit,test
+const User = require('./User');
+const Project = require('./Project');
+
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Project };
