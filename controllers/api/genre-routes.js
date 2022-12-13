78167f3ff74da26bd1a2b168b4f4a0936ccb2042
Andrew Connally <ajconnally93@gmail.com>
2022-12-12 20:04:35 -0600

    BUGFIXED the Genre table issue - Server is NOW LISTENING

--- controllers/api/genre-routes.js
+++ controllers/api/genre-routes.js
@@ -1,4 +1,4 @@
 
-const router = require("express").Router();
-const { Genres } = require("../../models");
+// const router = require("express").Router();
+// const { Genres } = require("../../models");
 
